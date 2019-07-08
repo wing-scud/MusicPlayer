53afa1deb37e2a551b9c98df1c5bf6a00994ba27
AIsay <35375466+Acemonia@users.noreply.github.com>
2019-07-08 19:12:54 +0800

    Update MusicRoomDatabase.kt

--- app/src/main/java/com/example/projectprepare1/database/MusicRoomDatabase.kt
+++ app/src/main/java/com/example/projectprepare1/database/MusicRoomDatabase.kt
@@ -19,6 +19,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import android.content.Context
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.android.roomwordssample.MusicDao
 import com.example.android.roomwordssample.Song
 import com.example.android.roomwordssample.SongList
@@ -54,12 +55,44 @@ abstract class MusicRoomDatabase : RoomDatabase() {
                     // Wipes and rebuilds instead of migrating if no Migration object.
                     // Migration is not part of this codelab.
                     .fallbackToDestructiveMigration()
-//                    .addCallback(WordDatabaseCallback(scope))
+                    .addCallback(WordDatabaseCallback(scope))
                     .build()
                 INSTANCE = instance
                 // return instance
                 instance
             }
+        }
+        private class WordDatabaseCallback(
+            private val scope: CoroutineScope
+        ) : RoomDatabase.Callback() {
+            /**
+             * Override the onOpen method to populate the database.
+             * For this sample, we clear the database every time it is created or opened.
+             */
+            override fun onOpen(db: SupportSQLiteDatabase) {
+                super.onOpen(db)
+                // If you want to keep the data through app restarts,
+                // comment out the following line.
+                INSTANCE?.let { database ->
+                    scope.launch {
+                        populateDatabase(database.musicDao())
+                    }
+                }
+            }
+        }
+
+        /**
+         * Populate the database in a new coroutine.
+         * If you want to start with more words, just add them.
+         */
+        fun populateDatabase(musicDao: MusicDao) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate on creation.
+//            musicDao.deleteAll()
+//            var word = Word("Hello")
+//            wordDao.insert(word)
+//            word = Word("World!")
+//            wordDao.insert(word)
         }
     }
 }
