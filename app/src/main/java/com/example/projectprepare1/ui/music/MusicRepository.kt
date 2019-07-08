6100203d758b02813adca3fe30f53ec29ee05039
AIsay <acemonia@gmail.com>
2019-07-08 16:27:47 +0800

    Add Music in entity, database and ui.

---
+++ app/src/main/java/com/example/projectprepare1/ui/music/MusicRepository.kt
@@ -0,0 +1,34 @@
+package com.example.android.roomwordssample
+
+import androidx.lifecycle.LiveData
+import com.example.android.roomwordssample.MusicDao
+import com.example.android.roomwordssample.Song
+
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Abstracted Repository as promoted by the Architecture Guide.
+ * https://developer.android.com/topic/libraries/architecture/guide.html
+ */
+class MusicRepository(private val musicDao: MusicDao) {
+    val allSongs: LiveData<List<Song>> = musicDao.getAllSongs()
+
+    suspend fun insert(song: Song) {
+        musicDao.insert(song)
+    }
+
+}
