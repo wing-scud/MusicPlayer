aafea54025772ac433c8c97c4745f2009a556e18
AIsay <acemonia@gmail.com>
2019-07-11 22:31:05 +0800

    Li

--- app/src/main/java/com/example/projectprepare1/MainActivity.kt
+++ app/src/main/java/com/example/projectprepare1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.projectprepare1
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.core.view.GravityCompat
@@ -11,6 +13,7 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -27,11 +30,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     val permissionUtils = ScanPermissionUtils(this)
     private var mHasPermissionRunnable: Runnable= Runnable {
     }
-    override fun onSupportNavigateUp(): Boolean {
-        return super.onSupportNavigateUp()
-        var fragment = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)!!;
-        return NavHostFragment.findNavController(fragment).navigateUp();
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        return super.onSupportNavigateUp()
+//        var fragment = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)!!;
+//        return NavHostFragment.findNavController(fragment).navigateUp();
+//    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -73,7 +76,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.action_settings ->{
-
+                Toast.makeText(this, "点击了选项", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent);
             }
         }
         return when (item.itemId) {
@@ -82,28 +87,27 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
+//下面函数没反应？？？
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view itemFragment clicks here.
         when (item.itemId) {
             R.id.nav_home -> {
+                Toast.makeText(this, "点击了登陆", Toast.LENGTH_SHORT).show();
+
                 // Handle the camera action
             }
-            R.id.nav_gallery -> {
-
+            R.id.nav_about-> {
+                Toast.makeText(this, "点击了登陆", Toast.LENGTH_SHORT).show();
+                System.exit(0)
             }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_tools -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
+            R.id.nav_out-> {
+//                onOptionsItemSelected(R.id.nav_out) {
+//                    System.exit(0)
+//                }
             }
         }
+
+        Toast.makeText(this, "点击了登陆", Toast.LENGTH_SHORT).show();
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
